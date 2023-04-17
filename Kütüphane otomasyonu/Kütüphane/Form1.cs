f7c7dc4caa34d82d2ac6f0fa7f0c51ca1996f58c
Furkancizreli35 <furkan.cizreli.mdbf21@iste.edu.tr>
2023-04-17 18:46:57 +0300

    kutuphane2

--- Kütüphane otomasyonu/Kütüphane/Form1.cs
+++ Kütüphane otomasyonu/Kütüphane/Form1.cs
@@ -12,9 +12,35 @@ namespace Kütüphane
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+    public Form1()
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void personelgirisbtn_Click(object sender, EventArgs e)
+        {
+            string gelenad = adgiristxt.Text;
+            string gelensifre = sifregiristxt.Text;
+            
+
+            if(gelenad.Equals("furkan.cizreli")&& gelensifre.Equals("123"))
+            {
+                MessageBox.Show("Giriş Başarılı");
+                Panel p = new Panel();
+                p.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre yanlış!");
+
+            }
+
+        }
+            
+        }        
+            
+        }
+    
+
