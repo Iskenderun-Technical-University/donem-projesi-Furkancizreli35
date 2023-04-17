5fe3b2dad483a657ba72b8d9581fd6b8a5dfeab0
Furkancizreli35 <128590184+Furkancizreli35@users.noreply.github.com>
2023-04-17 18:50:25 +0300

    Add files via upload

---
+++ Kutuphane 2/Kütüphane otomasyonu/Kütüphane/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Kütüphane
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.adgiristxt = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sifregiristxt = new System.Windows.Forms.TextBox();
+            this.personelgirisbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adgiristxt
+            // 
+            this.adgiristxt.Location = new System.Drawing.Point(163, 158);
+            this.adgiristxt.Name = "adgiristxt";
+            this.adgiristxt.Size = new System.Drawing.Size(120, 20);
+            this.adgiristxt.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre :";
+            // 
+            // sifregiristxt
+            // 
+            this.sifregiristxt.BackColor = System.Drawing.SystemColors.Window;
+            this.sifregiristxt.Location = new System.Drawing.Point(161, 206);
+            this.sifregiristxt.Name = "sifregiristxt";
+            this.sifregiristxt.Size = new System.Drawing.Size(120, 20);
+            this.sifregiristxt.TabIndex = 4;
+            // 
+            // personelgirisbtn
+            // 
+            this.personelgirisbtn.BackColor = System.Drawing.SystemColors.Control;
+            this.personelgirisbtn.Location = new System.Drawing.Point(183, 242);
+            this.personelgirisbtn.Name = "personelgirisbtn";
+            this.personelgirisbtn.Size = new System.Drawing.Size(69, 23);
+            this.personelgirisbtn.TabIndex = 5;
+            this.personelgirisbtn.Text = "Giriş";
+            this.personelgirisbtn.UseVisualStyleBackColor = false;
+            this.personelgirisbtn.Click += new System.EventHandler(this.personelgirisbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(189, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(411, 361);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.personelgirisbtn);
+            this.Controls.Add(this.sifregiristxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.adgiristxt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox adgiristxt;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sifregiristxt;
+        private System.Windows.Forms.Button personelgirisbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Kutuphane 2/Kütüphane otomasyonu/Kütüphane/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane
+{
+    public partial class Form1 : Form
+    {
+
+    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void personelgirisbtn_Click(object sender, EventArgs e)
+        {
+            string gelenad = adgiristxt.Text;
+            string gelensifre = sifregiristxt.Text;
+            
+
+            if(gelenad.Equals("furkan.cizreli")&& gelensifre.Equals("123"))
+            {
+                MessageBox.Show("Giriş Başarılı");
+                Panel p = new Panel();
+                p.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre yanlış!");
+
+            }
+
+        }
+            
+        }        
+            
+        }
+    
+
